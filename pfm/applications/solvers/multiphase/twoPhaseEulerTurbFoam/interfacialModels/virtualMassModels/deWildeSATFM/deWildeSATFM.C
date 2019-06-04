5a01f8b66af4f44aab61848638a95a70f9fece6d
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2019-06-04 15:28:07 +0200

    changed limiting value for deWilde virtual mass force

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/virtualMassModels/deWildeSATFM/deWildeSATFM.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/virtualMassModels/deWildeSATFM/deWildeSATFM.C
@@ -117,11 +117,11 @@ Foam::tmp<Foam::volScalarField> Foam::virtualMassModels::deWildeSATFM::Cvm() con
        *((alpha1*alpha2*rho1*rho2)/(rho*rho))
     );
     // Limit virtual mass coefficient
-    Vm0.min(100.0);
+    Vm0.min(10.0);
     Vm0.max(0.0);
     
     return
-        Vm0*rho/(alpha1*rho2);
+        pos(pair_.dispersed() - residualAlpha_)*Vm0*rho/(alpha1*rho2);
 }
 
 
