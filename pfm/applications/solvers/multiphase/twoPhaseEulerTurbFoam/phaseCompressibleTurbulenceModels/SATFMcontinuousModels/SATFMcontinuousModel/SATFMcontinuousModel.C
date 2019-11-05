bd1e9d43bcfb307ff27590069f330629aa3d9a75
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2019-11-05 11:46:17 +0100

    changed dynamic adjustment of Cp to be compatible with periodic box simulations

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
@@ -921,17 +921,17 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
         CphiG_ = CphiGscalar_*Cmu_;
         
         // Currently no dynamic procedure for Cp
-        /*
         const volScalarField& p(mesh_.lookupObject<volScalarField>("p"));
         volScalarField rhom = rho*alpha + alpha1*rho1;
-        volVectorField gradp = fvc::grad(p);
+        volVectorField gradp = fvc::grad(p_rgh);
 
-        Cp_ = (gradp&gN_)/(rhom*(gN_&gN_));
+        Cp_ = (gradp&gN_)/(rhom*(gN_&gN_))
+            + dimensionedScalar("unity",dimensionSet(0,0,0,0,0),1.0);
         Cp_ = filterS(Cp_);
         Cp_.min(1.0);
         Cp_.max(0.1);
-        */
-        Cp_     = CpScalar_;
+        
+        //Cp_     = CpScalar_;
     } else {
         // the sign of xiPhiG should be opposite to the slip velocity
         volVectorField xiPhiGDir = uSlip/(mag(uSlip)+dimensionedScalar("small",dimensionSet(0,1,-1,0,0),1.e-7));
