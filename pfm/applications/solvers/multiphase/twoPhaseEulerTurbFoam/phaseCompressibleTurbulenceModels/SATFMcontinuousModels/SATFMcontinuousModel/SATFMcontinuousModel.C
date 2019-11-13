fc651ed715621439019c1c1f0a21da1c8b657041
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2019-11-13 08:20:38 -0500

    deactivated dynamic adjustment of Cp and Cmu

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
@@ -842,6 +842,7 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
         boundxiPhiG(xiPhiG_);
                
         // compute mixing length dynamically
+        /*
         volScalarField Lij      = filter_(alpha*magSqr(U))/alpha2f - magSqr(Uf);
         volScalarField magSqrDf = filter_(alpha*magSqr(D))/alpha2f;
         magSqrDf.max(SMALL);
@@ -856,7 +857,8 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
         
         Cmu_ = pos(scalar(1.0) - alpha_ - residualAlpha_)*sqrt(CmuT)
              + neg(scalar(1.0) - alpha_ - residualAlpha_)*CmuScalar_;
-        // Cmu_    = CmuScalar_;
+        */
+        Cmu_    = CmuScalar_;
         // dynamic procedure for Ceps
         /*
         volScalarField nu2 = mesh_.lookupObject<volScalarField>("thermo:mu." + phase_.name())/rho_;
@@ -883,6 +885,7 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
         CphiG_ = CphiGscalar_*Cmu_;
         
         // Currently no dynamic procedure for Cp
+        /*
         const volScalarField& p_rgh(mesh_.lookupObject<volScalarField>("p_rgh"));
         volScalarField rhom = rho*alpha + alpha1*rho1;
         volVectorField gradp = fvc::grad(p_rgh);
@@ -892,6 +895,7 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
         Cp_ = filterS(Cp_);
         Cp_.min(1.0);
         Cp_.max(0.1);
+        */
         // Cp_     = CpScalar_;
     } else {
         // the sign of xiPhiG should be opposite to the slip velocity
