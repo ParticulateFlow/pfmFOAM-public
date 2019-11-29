19156a51297f47ce981b2e3a5ab720b8aea59766
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2019-11-29 11:33:13 +0100

    corrected bc for R

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
@@ -1279,13 +1279,9 @@ void Foam::RASModels::SATFMdispersedModel::correct()
 
         forAll(patches, patchi)
         {
-            if (patches[patchi].type() == "wall")
+            if (!patches[patchi].couled())
             {
-                for (int i=0; i<3; i++) {
-                    for (int j=0; j<3; j++) {
-                        R1Bf[patchi].component(j+i*3) = 0;
-                    }
-                }
+                R1Bf[patchi] = dev(R1Bf[patchi]);
             }
         }
     }
