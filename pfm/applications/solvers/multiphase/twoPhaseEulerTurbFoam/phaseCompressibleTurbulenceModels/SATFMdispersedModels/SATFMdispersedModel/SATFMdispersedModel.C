0c84d7d2cfbe618421eed21358c20ff108eb7d34
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-01-28 07:46:10 +0100

    testing

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
@@ -1304,7 +1304,7 @@ void Foam::RASModels::SATFMdispersedModel::correct()
                                 - beta
                                   //*CphiS_
                                   *(
-                                       xiGS_*sqrt((max(kC_&eSum,kSmall)*km)
+                                       xiGS_*sqrt(max(kC_&eSum,kSmall)*km)
                                      - km
                                    )/(rho*km);
     
