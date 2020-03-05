808c8c94e7d6fbd24eb3cac43c7a75ab6db7b243
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-03-05 07:40:26 +0100

    remove filter from dragCorr

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/driftVelocityModels/driftVelocityModel/driftVelocityModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/driftVelocityModels/driftVelocityModel/driftVelocityModel.C
@@ -140,7 +140,7 @@ Foam::driftVelocityModel::KdUdrift() const
         + ((ud&eY)*min(0.99*mag(uSlipV&eY)/(mag(ud&eY)+uSmall),1.0))*eY
         + ((ud&eZ)*min(0.99*mag(uSlipV&eZ)/(mag(ud&eZ)+uSmall),1.0))*eZ;
     
-    dragCorr_ = -filterS((ud&uSlipV)/sqr(uSlip));
+    dragCorr_ = -((ud&uSlipV)/sqr(uSlip));
 
     // multiply drift velocity by drag coefficient
     return
