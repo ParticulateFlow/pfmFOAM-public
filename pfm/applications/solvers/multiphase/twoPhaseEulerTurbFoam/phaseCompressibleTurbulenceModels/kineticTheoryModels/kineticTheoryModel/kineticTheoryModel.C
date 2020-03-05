4c297372091aa9e2bfff3a01f3e4313882caaa4d
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-03-05 14:01:11 +0100

    activate diffusion again

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/kineticTheoryModel/kineticTheoryModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/kineticTheoryModel/kineticTheoryModel.C
@@ -478,7 +478,7 @@ void Foam::RASModels::kineticTheoryModel::correct()
             fvm::ddt(alpha, rho, Theta_)
           + fvm::div(alphaRhoPhi, Theta_)
           //+ fvc::SuSp(-(fvc::ddt(alpha, rho) + fvc::div(alphaRhoPhi)), Theta_)
-          //- fvm::laplacian((2.0/3.0)*kappa_, Theta_, "laplacian(kappa,Theta)")
+          - fvm::laplacian((2.0/3.0)*kappa_, Theta_, "laplacian(kappa,Theta)")
          ==
           - fvm::SuSp((2.0/3.0)*((PsCoeff*I) && gradU), Theta_)
           + (2.0/3.0)*(tau && gradU)
