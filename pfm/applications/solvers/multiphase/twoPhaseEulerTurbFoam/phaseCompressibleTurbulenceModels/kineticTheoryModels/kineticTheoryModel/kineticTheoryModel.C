f724bf7d9117d3d65a779223ed34d36fe5711d4b
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-03-18 10:28:27 +0100

    correct ps source term

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/kineticTheoryModel/kineticTheoryModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/kineticTheoryModel/kineticTheoryModel.C
@@ -493,8 +493,7 @@ void Foam::RASModels::kineticTheoryModel::correct()
             )
           - fvm::laplacian(kappa_, Theta_, "laplacian(kappa,Theta)")
          ==
-          //- fvm::SuSp(PsCoeff*trD, Theta_)
-            PsCoeff*mag(trD)*Theta_
+          - fvm::SuSp(PsCoeff*trD, Theta_)
           + rho
            *(
                 lambda_*sqr(trD)
