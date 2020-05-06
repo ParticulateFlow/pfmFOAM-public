e7642cb76fabfa8991babdc6fec6badd817f6358
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-05-06 12:36:57 +0200

    anisotropic R2 deactivated

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
@@ -1301,7 +1301,7 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
             }
         }
     } else {
-        R2_  = ((k_&eX)*(eX*eX) + (k_&eY)*(eY*eY) + (k_&eZ)*(eZ*eZ));
+        R2_  = 0*((k_&eX)*(eX*eX) + (k_&eY)*(eY*eY) + (k_&eZ)*(eZ*eZ));
     }
     
     R2_.correctBoundaryConditions();
