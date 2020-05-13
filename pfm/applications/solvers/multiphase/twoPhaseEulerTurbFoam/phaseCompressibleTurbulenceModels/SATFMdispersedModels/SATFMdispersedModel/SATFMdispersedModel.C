860e595582346ecb3ad1738cfde6b27dda76192a
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-05-13 16:11:30 +0200

    remove eps part

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
@@ -1363,7 +1363,7 @@ void Foam::RASModels::SATFMdispersedModel::correct()
                                    )
                                  + xiPhiDivU_
                                   *alpha
-                                  *sqrt(2.0*mag(fvc::laplacian(km)) + km/(Cmu_*sqr(deltaF_)));
+                                  *sqrt(2.0*mag(fvc::laplacian(km)) + 0*km/(Cmu_*sqr(deltaF_)));
     
     Info << "Computing alphaP2Mean (dispersed phase) ... " << endl;
     volScalarField alpha1(alpha);
