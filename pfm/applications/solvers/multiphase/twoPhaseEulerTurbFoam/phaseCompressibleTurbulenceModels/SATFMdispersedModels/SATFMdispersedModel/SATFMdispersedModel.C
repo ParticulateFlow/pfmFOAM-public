5347f6441c5538e038d7aeb6c6433fe3bef3d828
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-08-03 20:59:17 +0200

    changed pPrime

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMdispersedModels/SATFMdispersedModel/SATFMdispersedModel.C
@@ -603,7 +603,7 @@ Foam::RASModels::SATFMdispersedModel::pPrime() const
       * pos(alpha_-alphaMinFriction_)
       */
        dimensionedScalar("1e25", dimensionSet(1, -1, -2, 0, 0), 1e25)
-      *pow(Foam::max(alpha_ - 0.99*alphaMax_, scalar(0)), 9.0)
+      *pow(pos(alpha_-0.99*alphaMax_)*(alpha_ - 0.99*alphaMax_), 9.0)
     );
 
     volScalarField::Boundary& bpPrime =
