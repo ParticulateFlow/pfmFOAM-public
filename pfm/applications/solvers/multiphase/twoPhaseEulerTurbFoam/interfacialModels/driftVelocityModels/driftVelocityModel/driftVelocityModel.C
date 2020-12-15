211dbe97e3231550fb67c8a5f5236340b191c85d
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-12-15 20:13:21 +0100

    drift vel limit

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/driftVelocityModels/driftVelocityModel/driftVelocityModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/driftVelocityModels/driftVelocityModel/driftVelocityModel.C
@@ -134,7 +134,7 @@ Foam::driftVelocityModel::KdUdrift() const
     // Parmentier et al., AIChE J., 2012
     volScalarField magUd = mag(ud);
     magUd.max(SMALL);
-    ud *= min(magUd,sqrt(2.0)*uSlip)/magUd;
+    ud *= min(magUd,uSlip)/magUd;
     /*
     ud =  ((ud&eX)*min(0.99*mag(uSlipV&eX)/(mag(ud&eX)+uSmall),1.0))*eX
         + ((ud&eY)*min(0.99*mag(uSlipV&eY)/(mag(ud&eY)+uSmall),1.0))*eY
