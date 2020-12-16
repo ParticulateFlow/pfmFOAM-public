65ea9931ce2fc45f361c0a6c4651357145d553ed
Simon Schneiderbauer <simon.schneiderbauer@jku.at>
2020-12-16 18:02:31 +0100

    different limit

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/driftVelocityModels/driftVelocityModel/driftVelocityModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/driftVelocityModels/driftVelocityModel/driftVelocityModel.C
@@ -130,21 +130,21 @@ Foam::driftVelocityModel::KdUdrift() const
     volScalarField uSlip(mag(uSlipV));
     uSlip.max(SMALL);
     
+    dragCorr_ = -((ud&uSlipV)/sqr(uSlip));
+    dragCorr_.min(0.95);
+    dragCorr_.max(-0.95);
+    
     // limit turbulent dispersion force according to
     // Parmentier et al., AIChE J., 2012
     volScalarField magUd = mag(ud);
     magUd.max(SMALL);
-    ud *= min(magUd,0.97*uSlip)/magUd;
+    ud *= min(magUd,0.95*uSlip)/magUd;
     /*
     ud =  ((ud&eX)*min(0.99*mag(uSlipV&eX)/(mag(ud&eX)+uSmall),1.0))*eX
         + ((ud&eY)*min(0.99*mag(uSlipV&eY)/(mag(ud&eY)+uSmall),1.0))*eY
         + ((ud&eZ)*min(0.99*mag(uSlipV&eZ)/(mag(ud&eZ)+uSmall),1.0))*eZ;
     */
     // multiply drift velocity by drag coefficient
-    
-    dragCorr_ = -((ud&uSlipV)/sqr(uSlip));
-    dragCorr_.min(0.99);
-    dragCorr_.max(-0.99);
     
     return
          drag.K()
