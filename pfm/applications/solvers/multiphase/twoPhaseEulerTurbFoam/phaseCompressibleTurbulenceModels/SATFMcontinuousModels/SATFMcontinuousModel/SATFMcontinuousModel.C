724d99ec6e18626594c0ba878707a6b059de7c9c
Rauchenzauner <stefanie.rauchenzauner@jku.at>
2022-09-29 15:12:38 +0200

    Corrected Reynolds stress

--- pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
+++ pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/SATFMcontinuousModels/SATFMcontinuousModel/SATFMcontinuousModel.C
@@ -1435,7 +1435,7 @@ void Foam::RASModels::SATFMcontinuousModel::correct()
         }
         */
     } else {
-        R2_ = 0*((k_&eX)*(eX*eX) + (k_&eY)*(eY*eY) + (k_&eZ)*(eZ*eZ));
+        R2_  = ((k_&eX)*(eX*eX) + (k_&eY)*(eY*eY) + (k_&eZ)*(eZ*eZ));
     }
     boundStress(R2_);
     R2_.correctBoundaryConditions();
