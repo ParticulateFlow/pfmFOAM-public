ParticulateFlow/pfmFOAM-public

commit b62ce2e7225f3042e33183b27b2140a42c80d680

fix minor issues with power calculations etc

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/frictionalStressModel/ChialvoEtAl/ChialvoEtAlFrictionalStress.C
@@ -253,7 +253,7 @@ ChialvoEtAl::frictionalPressurePrime
     (
        aInt_
        *k_
-       *sqrt(2.0*sqrt(DD)*dp/sqrt(k_/(rho*dp)))
+       *sqrt(2.0*dp*sqrt(DD/(k_/(rho*dp))))
        /dp
     );
     volScalarField pInert
@@ -278,7 +278,7 @@ ChialvoEtAl::frictionalPressurePrime
        *(2.0/3.0)
        *aQSk_
        *k_
-       /(pow(max(alpha - alphaMax,alphaDeltaMin_), 1.0/3.0)*dp);
+       /(cbrt(max(alpha - alphaMax,alphaDeltaMin_))*dp);
 }
 
 
@@ -330,8 +330,8 @@ ChialvoEtAl::nu
                                  pow(
                                      I0_.value()
                                    / (
-                                         (2.0 * sqrt(0.5*(D[celli]&&D[celli])) * dp[celli])
-                                        /(sqrt(pf[celli])+SMALL)
+                                         2.0 * dp[celli] * sqrt((0.5*(D[celli]&&D[celli]))
+                                        /(pf[celli]+SMALL))
                                        + SMALL
                                      )
                                     ,1.5
@@ -349,6 +349,8 @@ ChialvoEtAl::nu
 
     volScalarField::Boundary& nufBf = nuf.boundaryFieldRef();
 
+    volScalarField& sqrt05 = sqrt(0.5);
+
     forAll(patches, patchi)
     {
         if (!patches[patchi].coupled()) {
@@ -364,7 +366,7 @@ ChialvoEtAl::nu
                                    pow(
                                        I0_.value()
                                      / (
-                                           (sqrt(0.5)*mag(U.boundaryField()[patchi].snGrad())
+                                           (sqrt05*mag(U.boundaryField()[patchi].snGrad())
                                           *dp.boundaryField()[patchi])
                                           /(sqrt(pf.boundaryField()[patchi])+SMALL)
                                          + SMALL
@@ -376,7 +378,7 @@ ChialvoEtAl::nu
                              )
                             * pf.boundaryField()[patchi]
                             / (
-                                 sqrt(0.5)*mag(U.boundaryField()[patchi].snGrad())
+                                 sqrt05*mag(U.boundaryField()[patchi].snGrad())
                                + SMALL
                               );
         }

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/frictionalStressModel/SchneiderbauerEtAl/SchneiderbauerEtAlFrictionalStress.C
@@ -240,7 +240,7 @@ SchneiderbauerEtAl::frictionalPressurePrime
         *(2.0/3.0)
         *aQSk_
         *k_
-        /(pow(max(alpha - alphaMax,alphaDeltaMin_), 1.0/3.0)*dp);
+        /(cbrt(max(alpha - alphaMax,alphaDeltaMin_))*dp);
 }
 
 
@@ -291,8 +291,8 @@ SchneiderbauerEtAl::nu
                             /(
                                  I0_.value()
                                / (
-                                     (2.0 * sqrt(0.5*(D[celli]&&D[celli])) * dp[celli])
-                                    /(sqrt(pf[celli])+SMALL)
+                                     2.0 * dp[celli] * sqrt((0.5*(D[celli]&&D[celli]))
+                                    /(pf[celli]+SMALL))
                                    + SMALL
                                  )
                                + 1.0
@@ -310,6 +310,8 @@ SchneiderbauerEtAl::nu
 
     volScalarField::Boundary& nufBf = nuf.boundaryFieldRef();
 
+    volScalarField& sqrt05 = sqrt(0.5);
+
     forAll(patches, patchi)
     {
         if (!patches[patchi].coupled()) {
@@ -324,7 +326,7 @@ SchneiderbauerEtAl::nu
                               /(
                                    I0_.value()
                                  / (
-                                       (sqrt(0.5)*mag(U.boundaryField()[patchi].snGrad())
+                                       (sqrt05*mag(U.boundaryField()[patchi].snGrad())
                                       *dp.boundaryField()[patchi])
                                       /(sqrt(pf.boundaryField()[patchi])+SMALL)
                                      + SMALL
@@ -334,7 +336,7 @@ SchneiderbauerEtAl::nu
                              )
                             * pf.boundaryField()[patchi]
                             / (
-                                 sqrt(0.5)*mag(U.boundaryField()[patchi].snGrad())
+                                 sqrt05*mag(U.boundaryField()[patchi].snGrad())
                                + SMALL
                               );
         }

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/EEqns.H
@@ -14,7 +14,7 @@
     if (mesh.foundObject<volScalarField>("dragCorr")) {
         Info<< "Applying drag correction..." << endl;
         const volScalarField& dragCorr = mesh.lookupObject<volScalarField>("dragCorr");
-        Kd *= (scalar(1.0) - dragCorr);
+        Kd *= (1.0 - dragCorr);
     }
 
     volVectorField dragEx = Kd*(U2 - U1);

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/EEqnsSATFM.H
@@ -12,7 +12,7 @@
     if (mesh.foundObject<volScalarField>("heatTransferCorr")) {
         Info<< "Applying heat transfer correction..." << endl;
         const volScalarField& heatTransferCorr = mesh.lookupObject<volScalarField>("heatTransferCorr");
-        Kh *= (scalar(1.0) - heatTransferCorr);
+        Kh *= (1.0 - heatTransferCorr);
     }
 
     Kd = fluid.Kd();
@@ -22,7 +22,7 @@
     if (mesh.foundObject<volScalarField>("dragCorr")) {
         Info<< "Applying drag correction..." << endl;
         const volScalarField& dragCorr = mesh.lookupObject<volScalarField>("dragCorr");
-        Kd *= (scalar(1.0) - dragCorr);
+        Kd *= (1.0 - dragCorr);
     }
 
     volVectorField dragEx = Kd*(U2 - U1);

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/dragModels/Tang/Tang.C
@@ -70,7 +70,7 @@ Foam::tmp<Foam::volScalarField> Foam::dragModels::Tang::CdRe() const
 
     volScalarField alpha2
     (
-        max(scalar(1) - pair_.dispersed(), pair_.continuous().residualAlpha())
+        max(1.0 - pair_.dispersed(), pair_.continuous().residualAlpha())
     );
 
     volScalarField Res(alpha2*pair_.Re());
@@ -84,14 +84,14 @@ Foam::tmp<Foam::volScalarField> Foam::dragModels::Tang::CdRe() const
     volScalarField F0
     (
         "F0",
-        10.0*alpha1/sqr(alpha2) + sqr(alpha2)*(scalar(1.0) + 1.5*sqrt(alpha1))
+        10.0*alpha1/sqr(alpha2) + sqr(alpha2)*(1.0 + 1.5*sqrt(alpha1))
     );
 
     volScalarField F1
     (
         "F1",
-        Res*(0.11*alpha1*(1.0 + alpha1) - 0.00456/pow(alpha2, 4.0)
-      + pow(ResLim, -0.343)*(0.169*alpha2 + 0.0644/pow(alpha2, 4.0)))
+        Res*(0.11*alpha1*(1.0 + alpha1) - 0.00456/pow4(alpha2)
+      + pow(ResLim, -0.343)*(0.169*alpha2 + 0.0644/pow4(alpha2)))
     );
 
     return 24.0*alpha2*(F0 + F1);

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/interfacialModels/dragModels/Tenneti/Tenneti.C
@@ -70,7 +70,7 @@ Foam::tmp<Foam::volScalarField> Foam::dragModels::Tenneti::CdRe() const
 
     volScalarField alpha2
     (
-        max(scalar(1) - pair_.dispersed(), pair_.continuous().residualAlpha())
+        max(1.0 - pair_.dispersed(), pair_.continuous().residualAlpha())
     );
 
     volScalarField Res(alpha2*pair_.Re());
@@ -78,20 +78,20 @@ Foam::tmp<Foam::volScalarField> Foam::dragModels::Tenneti::CdRe() const
     volScalarField F0  
     (
         "F0",
-         neg(Res - 1000)*(1.0/pow(alpha2, 2.0))*(1.0 + 0.15*pow(Res, 0.687))
-       + pos0(Res - 1000)*(1.0/pow(alpha2, 2.0))*(0.44/24.0)*Res
+         neg(Res - 1000)*(1.0/sqr(alpha2))*(1.0 + 0.15*pow(Res, 0.687))
+       + pos0(Res - 1000)*(1.0/sqr(alpha2))*(0.44/24.0)*Res
     );
 
     volScalarField F1
     (
         "F1",
-        5.81*(alpha1/pow(alpha2, 2.0)) + 0.48*(pow(alpha1, 1.0/3.0)/pow(alpha2, 3.0))
+        5.81*(alpha1/sqr(alpha2)) + 0.48*(cbrt(alpha1)/pow3(alpha2))
     );
 
     volScalarField F2
     (
         "F2",
-        Res*alpha2*pow(alpha1, 3.0)*(0.95 + (0.61*pow(alpha1, 3.0)/pow(alpha2, 2.0)))
+        Res*alpha2*pow3(alpha1)*(0.95 + (0.61*pow3(alpha1)/sqr(alpha2)))
     );
 
     return 24.0*alpha2*(F0 + F1 + F2);

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/kineticTheoryModel/kineticTheoryModel.C
@@ -581,7 +581,7 @@ void Foam::RASModels::kineticTheoryModel::correct()
                 1.0
               + 3.0*sqrt(0.5*alpha)
               + 2.11*alpha*log(alpha)
-              + 11.26*alpha*(1.0 - 5.1*alpha + 16.57*sqr(alpha) - 21.77*alpha*sqr(alpha))
+              + 11.26*alpha*(1.0 - 5.1*alpha + 16.57*sqr(alpha) - 21.77*pow3(alpha))
               - alpha*gs0_*log(0.01)
             );
             J1 *= Rdiss;
@@ -590,9 +590,9 @@ void Foam::RASModels::kineticTheoryModel::correct()
             (
                 neg(alpha - 0.4)
                *(1.0 + 3.0*sqrt(0.5*alpha) + 2.11*alpha*log(alpha)+17.14*alpha)
-               /(1.0 + 0.681*alpha - 8.48*sqr(alpha) + 8.16*alpha*sqr(alpha))
+               /(1.0 + 0.681*alpha - 8.48*sqr(alpha) + 8.16*pow3(alpha))
               + pos(alpha - 0.4)
-               *(10.0*alpha/(sqr(1.0 - alpha)*(1.0 - alpha)) + 0.7)
+               *(10.0*alpha/(pow3((1.0 - alpha))) + 0.7)
             );
             volScalarField Psi
             (

pfm/src/randomProcesses/turbulence/Ek.H
@@ -21,7 +21,7 @@ inline tmp<scalarField> Ek
 )
 {
     // Kolmogorov length scale
-    const scalar eta = pow(sqr(nu)*nu/epsilon,0.25);
+    const scalar eta = pow025(pow3(nu)/epsilon);
     // Turbulent length scale (Pope, 2000)
     const scalar L = pow(e,1.5)/epsilon;
     
@@ -41,7 +41,7 @@ inline tmp<scalarField> Ek
         )
        *exp
         (
-            -beta*(pow(sqr(sqr(k*eta))+sqr(sqr(cEta)),0.25) - cEta)
+            -beta*(pow025(pow4(k*eta)+pow4(cEta)) - cEta)
         )
        /(nu*pow(k + 1.0e-20,5.0/3.0))
     );

commit 7b1e9db5b1c2007037ab2f2dec42fc436d856704

fix incorrect type introduced in b62ce2e7225f3042e33183b27b2140a42c80d680

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/frictionalStressModel/ChialvoEtAl/ChialvoEtAlFrictionalStress.C
@@ -33,8 +33,8 @@ License
         Chialvo, S., J. Sun, S. Sundaresan. Phys. Rev. E, 2012, 85, 021305 (2012).
         Schneiderbauer, S., A. Aigner, S. Pirker. Chem. Eng. Sci., 2012, 80, 279–292.
      \endverbatim
- 
- 
+
+
 
 \*---------------------------------------------------------------------------*/
 
@@ -157,7 +157,7 @@ ChialvoEtAl::frictionalPressure
     }
     // Correct coupled BCs
     DD.correctBoundaryConditions();
-    
+
     volScalarField pInt
     (
        aInt_
@@ -201,7 +201,7 @@ ChialvoEtAl::frictionalPressurePrime
 ) const
 {
     const volScalarField& alpha = phase;
-    
+
     // compute D&&D in the interior and at boundaries
     tmp<volScalarField> tDD
     (
@@ -248,7 +248,7 @@ ChialvoEtAl::frictionalPressurePrime
     }
     // Correct coupled BCs
     DD.correctBoundaryConditions();
-    
+
     volScalarField pInt
     (
        aInt_
@@ -349,7 +349,7 @@ ChialvoEtAl::nu
 
     volScalarField::Boundary& nufBf = nuf.boundaryFieldRef();
 
-    volScalarField& sqrt05 = sqrt(0.5);
+    const scalar sqrt05 = sqrt(0.5);
 
     forAll(patches, patchi)
     {
@@ -404,7 +404,7 @@ ChialvoEtAl::read()
     aQSk_.read(coeffDict_);
     aInt_.read(coeffDict_);
     k_.read(coeffDict_);
-    
+
     alphaDeltaMin_.read(coeffDict_);
     Rc_.read(coeffDict_);
 

pfm/applications/solvers/multiphase/twoPhaseEulerTurbFoam/phaseCompressibleTurbulenceModels/kineticTheoryModels/frictionalStressModel/SchneiderbauerEtAl/SchneiderbauerEtAlFrictionalStress.C
@@ -29,8 +29,8 @@ License
  Description
      Implementaton of mu(I)-rheology (Schneiderbauer et al., Chem. Eng.
      Science, 80, 2012)
- 
- 
+
+
 
 \*---------------------------------------------------------------------------*/
 
@@ -105,7 +105,7 @@ SchneiderbauerEtAl::frictionalPressure
 ) const
 {
     const volScalarField& alpha = phase;
-    
+
     // compute D&&D in the interior and at boundaries
     tmp<volScalarField> tDD
     (
@@ -183,7 +183,7 @@ SchneiderbauerEtAl::frictionalPressurePrime
 ) const
 {
     const volScalarField& alpha = phase;
-    
+
     // compute D&&D in the interior and at boundaries
     tmp<volScalarField> tDD
     (
@@ -231,7 +231,7 @@ SchneiderbauerEtAl::frictionalPressurePrime
     }
     // Correct coupled BCs
     DD.correctBoundaryConditions();
-    
+
     return
        - pos(alpha - alphaMinFriction)
         *8.0*rho*sqr(b_*dp)*DD
@@ -310,7 +310,7 @@ SchneiderbauerEtAl::nu
 
     volScalarField::Boundary& nufBf = nuf.boundaryFieldRef();
 
-    volScalarField& sqrt05 = sqrt(0.5);
+    const scalar sqrt05 = sqrt(0.5);
 
     forAll(patches, patchi)
     {
@@ -360,7 +360,7 @@ SchneiderbauerEtAl::read()
     I0_.read(coeffDict_);
     aQSk_.read(coeffDict_);
     k_.read(coeffDict_);
-    
+
     alphaDeltaMin_.read(coeffDict_);
     Rc_.read(coeffDict_);
 